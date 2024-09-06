8bf327cd20419341f8e7c53c59a339e6d7ef8ba9
Albert Lee <grepinsight@gmail.com>
2024-09-07 08:20:17 +0900

    feat: add file size utility functions and tests
    - Add `get_human_readable_size` function to convert file size to a human-readable format.
    - Add `print_file_size` function to print the file size in a human-readable format.
    - Update version from 0.2.0 to 0.2.1 in `__about__.py`.
    - Remove dependencies from `pyproject.toml`.
    - Add unit tests for the new file size utility functions in `tests/test_io.py`.

--- albylib/io.py
+++ albylib/io.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from typing import Union
+import os
 
 import pandas as pd
 
@@ -10,3 +11,15 @@ def read_glob(path: Union[str, Path], glob_pattern: str):
         d = pd.read_table(i)
         ds.append(d)
     return pd.concat(ds).reset_index(drop=True)
+
+
+def get_human_readable_size(size_in_bytes):
+    for unit in ["B", "KB", "MB", "GB", "TB", "PB"]:
+        if size_in_bytes < 1024:
+            return f"{size_in_bytes:.2f} {unit}"
+        size_in_bytes /= 1024
+
+
+def print_file_size(file_path: Union[str, Path]):
+    file_size = os.path.getsize(file_path)
+    print(f"File size: {get_human_readable_size(file_size)}")
